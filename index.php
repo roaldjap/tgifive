a1b698bc72efb58c15814b45b152100c019c9fd8
Roald Ignacio <me@mac.com>
2017-11-18 17:12:26 +0800

    added php

---
+++ index.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <!-- FB OG Meta -->
+    <meta property="og:image" content="https://roaldjap.github.io/tgifive/img/facebook.png"/>
+    <meta property="og:title" content="Thank God It's Five - December 10, 2017 - Save the date!"/>
+    <meta property="og:description" content="Let us celebrate God's goodness in our 5th anniversary. Together with your friends and family, Let's praise Him and continue to reveal His glory.">
+    <meta property="og:url" content="https://roaldjap.github.io/tgifive/"/>
+    <meta property="og:site_name" content="Thank God It's Five! - Victory Meycauayan's 5th Anniversary"/>
+    <meta property="og:type" content="website"/>
+
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
+    <meta name="viewport" content="initial-scale = 1.0,maximum-scale = 1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Thank God It's Five - 5th Anniversary of Victory Meycauayan</title>
+    <link rel="apple-touch-icon" sizes="57x57" href="img/favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="img/favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="img/favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="img/favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="img/favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="img/favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="img/favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="img/favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="img/favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="img/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="img/favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="img/favicon/favicon-16x16.png">
+    <link rel="manifest" href="img/favicon/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="img/favicon/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+
+    
+    
+
+    <link rel="stylesheet" href="css/jquery.classycountdown.css"/>
+    <link rel="stylesheet" href="bower_components/animate.css/animate.min.css">
+    <link rel="stylesheet" href="bower_components/Ionicons/css/ionicons.min.css">
+    <link rel="stylesheet" href="css/style.css"/>
+    
+    <script src="bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="bower_components/jquery-knob/dist/jquery.knob.min.js"></script>
+    <script src="js/jquery.throttle.js"></script>
+    <script src="js/jquery.classycountdown.js"></script>
+    <script src="bower_components/imagesloaded/imagesloaded.pkgd.min.js"></script>
+        
+    <script>
+        $(document).ready(function(){
+            
+            var isMobile = false; //initiate as false
+            // device detection
+            if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
+            || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) isMobile = true;
+            console.log(isMobile);
+            $('.pattern').imagesLoaded( { background: true }, function() {
+                var timestamp = new Date().getTime();
+                $('.tgif-logo').attr("src", "img/logo.gif"+'?'+timestamp );
+                $('.pattern, .player').addClass('slide');
+                $('.content-wrapper').addClass("animated fadeInUp");
+
+                $('#countdown').ClassyCountdown({
+                    end:  Math.round( new Date( 'Sun Dec 10 2017 00:00:00 GMT+0800' ).getTime() / 1000),
+                    now:  Math.round( new Date( ).getTime() / 1000),
+                    labels: true,
+                    style: {
+                        element: "",
+                        textResponsive: .5,
+                        days: {
+                            gauge: {
+                                thickness: .05,
+                                bgColor: "rgba(255,255,255,.25)",
+                                fgColor: "#41b54e",
+                                lineCap: 'round'
+                            },
+                            textCSS: 'font-family:\'Avenir Next\'; font-size:12px; font-weight:700; color:#14488a;'
+                        },
+                        hours: {
+                            gauge: {
+                                thickness: .05,
+                                bgColor: "rgba(255,255,255,.25)",
+                                fgColor: "#ebacce",
+                                lineCap: 'round'
+                            },
+                            textCSS: 'font-family:\'Avenir Next\'; font-size:12px; font-weight:700; color:#14488a;'
+                        },
+                        minutes: {
+                            gauge: {
+                                thickness: .05,
+                                bgColor: "rgba(255,255,255,.25)",
+                                fgColor: "#8e44ad",
+                                lineCap: 'round'
+                            },
+                            textCSS: 'font-family:\'Avenir Next\'; font-size:12px; font-weight:700; color:#14488a;'
+                        },
+                        seconds: {
+                            gauge: {
+                                thickness: .05,
+                                bgColor: "rgba(255,255,255,.25)",
+                                fgColor: "#f39c12",
+                                lineCap: 'round'
+                            },
+                            textCSS: 'font-family:\'Avenir Next\'; font-size:12px; font-weight:700; color:#14488a;'
+                        }
+
+                    },
+                    onEndCallback: function() {
+                        console.log("Thank God for His goodness and faithfulness!" + $.now());
+
+                    }
+                });
+
+                var myaudio = new Audio('audio/audio-1.mp3');
+
+                myaudio.volume = 0.5;
+                myaudio.autoplay = true;
+                myaudio.loop = true;
+
+                if(isMobile){
+                    myaudio.volume = 1;
+                    myaudio.pause();
+                    $('#mute-toggle').children('.icon').removeClass('ion-volume-medium');
+                    $('#mute-toggle').children('.icon').addClass('ion-volume-mute');
+                }
+
+                $('#mute-toggle').click(function(e) {
+                    e.preventDefault();
+                    
+                    if(myaudio.paused){
+                        myaudio.play();
+                        $(this).children('.icon').removeClass('ion-volume-mute');
+                        $(this).children('.icon').addClass('ion-volume-medium');
+                        console.log("play");
+                    } else{
+                        myaudio.pause();
+                        $(this).children('.icon').removeClass('ion-volume-medium');
+                        $(this).children('.icon').addClass('ion-volume-mute');
+
+                    }
+
+                });
+
+            });
+
+        
+        });
+    
+    
+    </script>
+       
+</head>
+
+<body>
+    <div class="pattern top"></div>
+    <div class="pattern bottom"></div>
+    <!-- player -->
+    <div class="player">
+            <a class="button" id="mute-toggle" href="" title=""><i class="icon ion-volume-medium"></i></a>            
+    </div>
+
+    <!-- content -->
+    <div class="container">
+        <span class="valign"></span>
+        <img class="tgif-logo" src="img/logo-spacer.png" alt="">
+        <div class="content-wrapper">
+            <p class="details"><span class="event">Victory Meycauayan's 5th Anniversary </span><span class="date">December 10, 2017</span><span class="place">Novotel Araneta Center Cubao, Q.C.</span></p>
+            <div class="countdown-wrapper">
+                <div id="countdown"></div>
+            </div>
+        </div>
+        
+    </div>
+
+
+
+    <!-- <script>
+        window.fbAsyncInit = function() {
+          FB.init({
+            appId      : '267319687124259',
+            cookie     : true,
+            xfbml      : true,
+            version    : '2.11'
+          });
+            
+          FB.AppEvents.logPageView();   
+          
+        };
+      
+        (function(d, s, id){
+           var js, fjs = d.getElementsByTagName(s)[0];
+           if (d.getElementById(id)) {return;}
+           js = d.createElement(s); js.id = id;
+           js.src = "https://connect.facebook.net/en_US/sdk.js";
+           fjs.parentNode.insertBefore(js, fjs);
+         }(document, 'script', 'facebook-jssdk'));
+      </script> -->
+</body>
+</html>
